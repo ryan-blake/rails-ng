bccf2b91c93181d7edf112069ffe1a2bc57648ca
Ryan Kliemann <Ryan_blake@icloud.com>
2016-08-18 16:22:07 -0500

    set up angular and bootstrap dependencies to rails and routing

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,19 @@
+angular
+  .module('app', ['ui.router', 'templates'])
+  .config(['$stateProvider', '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+      $stateProvider
+        .state('/', {
+          url:'/',
+          templateUrl: 'main/_home.html',
+          controller: 'MainCtrl'
+        })
+        .state('page', {
+          url:'/page',
+          templateUrl:'main/_page.html',
+          contrller: 'MainCtrl'
+        });
+
+        $urlRouterProvider.otherwise('/')
+
+    }]);
---
+++ app/assets/javascripts/main/MainCtrl.js
@@ -0,0 +1,12 @@
+(function(){
+
+
+angular
+  .module('app')
+  .controller('MainCtrl', MainCtrl);
+
+function MainCtrl(){
+
+
+}
+})();
