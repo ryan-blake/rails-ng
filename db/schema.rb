213f8c5925c925cf4f184426fdca5466bc510cb6
Ryan Kliemann <Ryan_blake@icloud.com>
2016-08-20 16:32:30 -0500

    ditch posts in current schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819212643) do
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
